f07ad2cefaa982924abf88eb705ec0cfc599dffc
M1LO225 <emilio.guerrero.argoti@udla.edu.ec>
2024-10-28 07:26:27 -0500

    Primer commit, se creó el primer modulo de GuerreroEmilio

---
+++ EGuerrero_ExamenProgreso1/Models/GuerreroEmilio.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EGuerrero_ExamenProgreso1.Models
+{
+    public class GuerreroEmilio
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        [MaxLength(30, ErrorMessage = "El nombre no puede ser de mas de 30 caracteres")]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(30)]
+        [MaxLength(30, ErrorMessage = "El apellido no puede ser de mas de 30 caracteres")]
+        public string Apellido { get; set; }
+        [Display(Name = "Tiene experiencia en este trabajo?")]
+        public bool TieneExperiencia { get; set; }
+        
+        public DateTime FechaNacimiento { get; set; }
+        public double Salario { get; set; }
+
+    }
+}
